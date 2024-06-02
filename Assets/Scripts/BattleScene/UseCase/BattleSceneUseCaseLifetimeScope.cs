edd2937fb55c89a749f9b417473de1c296fb09db
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-06-02 19:05:26 +0900

    DIコンテナを更新

--- Assets/Scripts/BattleScene/UseCase/BattleSceneUseCaseLifetimeScope.cs
+++ Assets/Scripts/BattleScene/UseCase/BattleSceneUseCaseLifetimeScope.cs
@@ -2,6 +2,22 @@
 using BattleScene.UseCase.Event;
 using BattleScene.UseCase.Event.Runner;
 using BattleScene.UseCase.Service;
+using BattleScene.UseCase.View.AilmentView.OutputDataFactory;
+using BattleScene.UseCase.View.AttackCountView.OutputDataFactory;
+using BattleScene.UseCase.View.BuffView.OutputDataFactory;
+using BattleScene.UseCase.View.CharacterVibesView.OutputDataFactory;
+using BattleScene.UseCase.View.DestroyedPartView.OutputDataFactory;
+using BattleScene.UseCase.View.DigitView.OutputDataFactory;
+using BattleScene.UseCase.View.EnemyView.OutputDataFactory;
+using BattleScene.UseCase.View.FrameView.OutputDataFactory;
+using BattleScene.UseCase.View.HitPointBarView.OutputDataFactory;
+using BattleScene.UseCase.View.IsContinueView.OutputDataFactory;
+using BattleScene.UseCase.View.MessageView.OutputDataFactory;
+using BattleScene.UseCase.View.OrderView.OutputDataFactory;
+using BattleScene.UseCase.View.PlayerImageView.OutputDataFactory;
+using BattleScene.UseCase.View.SelectActionView.OutputDataFactory;
+using BattleScene.UseCase.View.SelectSkillView.OutputDataFactory;
+using BattleScene.UseCase.View.TechnicalPointBarView.OutputDaraFactory;
 using VContainer;
 using VContainer.Unity;
 
@@ -57,8 +73,41 @@ namespace BattleScene.UseCase
             builder.Register<ToBodyPartNumberService>(Lifetime.Singleton);
             builder.Register<ToBuffNumberService>(Lifetime.Singleton);
             builder.Register<TurnService>(Lifetime.Singleton);
-            builder.Register<CharactersDomainService>(Lifetime.Singleton);
 
+            builder.Register<AilmentDomainService>(Lifetime.Singleton);
+            builder.Register<BodyPartDomainService>(Lifetime.Singleton);
+            builder.Register<BuffDomainService>(Lifetime.Singleton);
+            builder.Register<CharactersDomainService>(Lifetime.Singleton);
+            builder.Register<HitPointDomainService>(Lifetime.Singleton);
+            builder.Register<OrderedItemsDomainService>(Lifetime.Singleton);
+            builder.Register<ResultCreatorDomainService>(Lifetime.Singleton);
+            builder.Register<ResultDomainService>(Lifetime.Singleton);
+            builder.Register<SlipDamageDomainService>(Lifetime.Singleton);
+            builder.Register<TargetDomainService>(Lifetime.Singleton);
+
+            builder.Register<AilmentOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<AttackCountOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<BuffOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<CharacterVibesOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<DestroyedPartOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<CureDigitOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<DamageDigitOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<EnemyOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<TargetFrameOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<HitPointBarOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<IsContinueOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<AilmentMessageOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<DamageMessageOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<MessageOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<SelectSkillMessageOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<OrderOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<AilmentPlayerImageOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<PlayerAttackPlayerImageOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<PlayerImageOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<SelectSkillPlayerImageOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<SelectActionOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<SelectSkillOutputDataFactory>(Lifetime.Singleton);
+            builder.Register<TechnicalPointBarOutputDataFactory>(Lifetime.Singleton);
         }
     }
 }
