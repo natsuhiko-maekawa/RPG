53fb17357f00dbdee182122df6eb1fe3f7353403
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-06-04 07:53:13 +0900

    1フレームに実行する処理を表すstructを追加

---
+++ Assets/Scripts/BattleScene/UseCase/Event/Runner/EventStruct.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace BattleScene.UseCase.Event.Runner
+{
+    public struct EventStruct
+    {
+        private ImmutableList<EventCode> EventCodeList { get; }
+
+        // TODO: Controller, Presenterも追加する
+        
+        public EventStruct(IList<EventCode> eventCodeList)
+        {
+            EventCodeList = eventCodeList.ToImmutableList();
+        }
+    }
+}
