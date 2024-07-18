cc3b621d9fcb27d5d2d279683f22ff8887f5ab82
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-07-19 08:21:29 +0900

    ファイルを整理

--- Assets/Scripts/BattleScene/Domain/ValueObject/PlayerViewInfoValueObject.cs
+++ Assets/Scripts/BattleScene/Domain/ValueObject/PlayerViewInfoValueObject.cs
@@ -1,12 +1,27 @@
-﻿// namespace BattleScene.Domain.ValueObject
-// {
-//     public class PlayerViewInfoValueObject
-//     {
-//         public PlayerViewInfoValueObject(string playerName)
-//         {
-//             PlayerName = playerName;
-//         }
-//
-//         public string PlayerName { get; }
-//     }
-// }
+﻿using System;
+using BattleScene.Domain.Code;
+using UnityEngine;
+
+namespace BattleScene.InterfaceAdapter.DataAccess.Factory.Dto
+{
+
+    [Serializable]
+    public class PlayerViewInfoValueObject : IUniqueItem<CharacterTypeId>, ISerializationCallbackReceiver
+    {
+        [SerializeField] private string playerImageCode;
+        [SerializeField] private string playerName;
+        public CharacterTypeId Id { get; private set; }
+        public string PlayerName { get; private set; }
+    
+        public void OnBeforeSerialize()
+        {
+        }
+
+        public void OnAfterDeserialize()
+        {
+            Id = Enum.Parse<CharacterTypeId>(playerImageCode);
+            PlayerName = playerName;
+        }
+    }
+    
+}
