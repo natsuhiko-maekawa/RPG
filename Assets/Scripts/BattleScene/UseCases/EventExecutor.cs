4bb41dc26b6b5cb5e11622eb6d89d282fa2df82d
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-07-18 11:01:19 +0900

    EventExecutorを修正

--- Assets/Scripts/BattleScene/UseCases/EventExecutor.cs
+++ Assets/Scripts/BattleScene/UseCases/EventExecutor.cs
@@ -1,12 +1,26 @@
-﻿using BattleScene.UseCases.StateMachine;
+﻿using System;
+using BattleScene.UseCases.Event;
+using BattleScene.UseCases.StateMachine;
+using VContainer;
 
 namespace BattleScene.UseCases
 {
     public class EventExecutor
     {
+        private readonly IObjectResolver _container;
+
+        public EventExecutor(IObjectResolver container)
+        {
+            _container = container;
+        }
+
         public StateCode Execute(StateCode stateCode)
         {
-            return stateCode;
+            return stateCode switch
+            {
+                StateCode.Initialize => _container.Resolve<InitializationEvent>().GetStateCode(),
+                _ => throw new ArgumentOutOfRangeException()
+            };
         }
     }
 }
