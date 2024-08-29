8304c2fa38c5cc5e9dc6acf26f1e1845570d0dbd
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-08-30 08:11:11 +0900

    DamageMessageStateを追加

---
+++ Assets/Scripts/BattleScene/UseCases/StateMachine/SkillStateMachine/DamageMessageState.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleScene.UseCases.StateMachine.SkillStateMachine
+{
+    public class DamageMessageState : AbstractSkillState
+    {
+        public override void Start()
+        {
+            base.Start();
+        }
+    }
+}
