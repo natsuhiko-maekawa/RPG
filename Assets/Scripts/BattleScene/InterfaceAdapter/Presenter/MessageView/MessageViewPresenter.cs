3509f735df112c4c6b5bed78e6438a4cfa62e4bc
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-08-30 07:53:58 +0900

    MessageViewPresenterを修正

--- Assets/Scripts/BattleScene/InterfaceAdapter/Presenter/MessageView/MessageViewPresenter.cs
+++ Assets/Scripts/BattleScene/InterfaceAdapter/Presenter/MessageView/MessageViewPresenter.cs
@@ -1,4 +1,6 @@
-﻿using BattleScene.InterfaceAdapter.IView;
+﻿using BattleScene.Domain.Code;
+using BattleScene.Domain.IFactory;
+using BattleScene.InterfaceAdapter.IView;
 using BattleScene.InterfaceAdapter.Service;
 using BattleScene.UseCases.View.MessageView.OutputBoundary;
 using BattleScene.UseCases.View.MessageView.OutputData;
@@ -7,13 +9,16 @@ namespace BattleScene.InterfaceAdapter.Presenter.MessageView
 {
     public class MessageViewPresenter : IMessageViewPresenter
     {
+        private readonly IFactory<string, MessageCode> _messageFactory;
         private readonly MessageCodeConverterService _messageCodeConverter;
         private readonly IMessageView _messageView;
 
         public MessageViewPresenter(
+            IFactory<string, MessageCode> messageFactory,
             MessageCodeConverterService messageCodeConverter,
             IMessageView messageView)
         {
+            _messageFactory = messageFactory;
             _messageCodeConverter = messageCodeConverter;
             _messageView = messageView;
         }
@@ -22,6 +27,13 @@ namespace BattleScene.InterfaceAdapter.Presenter.MessageView
         {
         }
 
+        public void Start(MessageCode messageCode, bool noWait = false)
+        {
+            _messageView.StopAnimation();
+            var message = _messageFactory.Create(messageCode);
+            
+        }
+        
         public void Start(MessageOutputData outputData)
         {
             _messageView.StopAnimation();
