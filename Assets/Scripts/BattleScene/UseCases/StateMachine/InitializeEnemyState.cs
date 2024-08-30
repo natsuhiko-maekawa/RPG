20e668bb3e90fdecd32e6fc266656b1d16e59c78
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-08-30 22:59:50 +0900

    InitializeEnemyStateを修正

--- Assets/Scripts/BattleScene/UseCases/StateMachine/InitializeEnemyState.cs
+++ Assets/Scripts/BattleScene/UseCases/StateMachine/InitializeEnemyState.cs
@@ -1,30 +1,39 @@
-﻿using BattleScene.UseCases.UseCase;
+﻿using System.Collections.Generic;
+using BattleScene.Domain.Code;
+using BattleScene.Domain.DomainService;
 using BattleScene.UseCases.View.EnemyView;
 using VContainer;
+using static BattleScene.Domain.Code.CharacterTypeCode;
 
 namespace BattleScene.UseCases.StateMachine
 {
     internal class InitializeEnemyState : AbstractState
     {
-        private readonly BattleStart _battleStart;
+        private readonly EnemiesDomainService _enemies;
         private readonly EnemyViewOutput _enemyView;
         private readonly IObjectResolver _container;
 
         public InitializeEnemyState(
-            BattleStart battleStart,
+            EnemiesDomainService enemies,
             EnemyViewOutput enemyView,
             IObjectResolver container)
         {
-            _battleStart = battleStart;
+            _enemies = enemies;
             _enemyView = enemyView;
             _container = container;
         }
 
         public override void Start()
         {
-            _battleStart.Execute();
+            SetEnemies();
             _enemyView.Out();
             Context.TransitionTo(_container.Resolve<OrderState>());
+        }
+
+        private void SetEnemies()
+        {
+            var enemyTypeIdList = new List<CharacterTypeCode> { Bee, Dragon, Mantis, Shuten, Slime };
+            _enemies.Add(enemyTypeIdList);
         }
     }
 }
