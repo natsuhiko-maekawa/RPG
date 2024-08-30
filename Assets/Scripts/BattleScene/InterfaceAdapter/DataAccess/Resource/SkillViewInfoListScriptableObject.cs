999c4d234ad7544bcc8ba71d95c312376cad0be6
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-08-30 12:10:26 +0900

    アセットメニューを追加

--- Assets/Scripts/BattleScene/InterfaceAdapter/DataAccess/Resource/SkillViewInfoListScriptableObject.cs
+++ Assets/Scripts/BattleScene/InterfaceAdapter/DataAccess/Resource/SkillViewInfoListScriptableObject.cs
@@ -1,8 +1,10 @@
 ﻿using BattleScene.Domain.Code;
 using BattleScene.Domain.ValueObject;
+using UnityEngine;
 
 namespace BattleScene.InterfaceAdapter.DataAccess.Resource
 {
+    [CreateAssetMenu(menuName = "ScriptableObjects/SkillViewInfo")]
     public class SkillViewInfoListScriptableObject : BaseListScriptableObject<SkillViewInfoValueObject, SkillCode>
     {
     }
