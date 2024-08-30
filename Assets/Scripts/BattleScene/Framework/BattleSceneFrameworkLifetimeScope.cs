1c1b28741cb0be2657ec67700ac9ccf549696f08
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-08-30 18:16:21 +0900

    DIコンテナから依存関係を削除

--- Assets/Scripts/BattleScene/Framework/BattleSceneFrameworkLifetimeScope.cs
+++ Assets/Scripts/BattleScene/Framework/BattleSceneFrameworkLifetimeScope.cs
@@ -20,8 +20,6 @@ namespace BattleScene.Framework
             builder.RegisterComponentInHierarchy<IPlayerStatusView>();
             builder.RegisterComponentInHierarchy<ISelectActionView>();
             builder.RegisterComponentInHierarchy<ISelectSkillView>();
-            // builder.RegisterComponentInHierarchy<IAilmentViewInfoResource>();
-            builder.RegisterComponentInHierarchy<IEnemyViewInfoResource>();
             builder.RegisterComponentInHierarchy<IPlayerPropertyResource>();
             builder.RegisterComponentInHierarchy<IPropertyResource>();
         }
