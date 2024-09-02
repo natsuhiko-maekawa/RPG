5d3885200a5a29662673901b1da24484318130bd
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-02 12:43:01 +0900

    不要なメソッドを削除

--- Assets/Scripts/BattleScene/UseCases/Service/DamageGeneratorService.cs
+++ Assets/Scripts/BattleScene/UseCases/Service/DamageGeneratorService.cs
@@ -10,7 +10,6 @@ namespace BattleScene.UseCases.Service
     public class DamageGeneratorService
     {
         private readonly OrderedItemsDomainService _orderedItems;
-        private readonly ResultCreatorDomainService _resultCreator;
         private readonly DamageEvaluatorService _damageEvaluator;
         private readonly IsHitEvaluatorService _isHitEvaluator;
         private readonly AttacksWeakPointEvaluatorService _attacksWeakPointEvaluator;
@@ -18,14 +17,12 @@ namespace BattleScene.UseCases.Service
 
         public DamageGeneratorService(
             OrderedItemsDomainService orderedItems,
-            ResultCreatorDomainService resultCreator,
             DamageEvaluatorService damageEvaluator,
             IsHitEvaluatorService isHitEvaluator,
             AttacksWeakPointEvaluatorService attacksWeakPointEvaluator,
             TargetDomainService target)
         {
             _orderedItems = orderedItems;
-            _resultCreator = resultCreator;
             _damageEvaluator = damageEvaluator;
             _isHitEvaluator = isHitEvaluator;
             _attacksWeakPointEvaluator = attacksWeakPointEvaluator;
@@ -61,63 +58,8 @@ namespace BattleScene.UseCases.Service
                 damageList.ToImmutableList());
         }
         
-        public ResultEntity Execute(SkillValueObject skill, DamageParameterValueObject damageParameterInfo)
-        {
-            _orderedItems.First().TryGetCharacterId(out var actorId);
-            var damageList = new List<AttackValueObject>();
-            for (var i = 0; i < damageParameterInfo.AttackNumber; ++i)
-            {
-                var targetIdList = _target.Get(actorId, skill.Range);
-                foreach (var targetId in targetIdList)
-                {
-                    var damage = new AttackValueObject(
-                        amount: _damageEvaluator.Evaluate(actorId, targetId, damageParameterInfo),
-                        isHit: _isHitEvaluator.Evaluate(actorId, targetId, damageParameterInfo),
-                        attacksWeakPoint: _attacksWeakPointEvaluator.Evaluate(actorId, targetId, damageParameterInfo),
-                        targetId: targetId,
-                        number: i);
-                    damageList.Add(damage);
-                }
-            }
-
-            damageList.Sort((x, y) => x.Number - y.Number);
-            var damageSkillResult = new DamageValueObject(
-                actorId,
-                skill.SkillCode,
-                damageList.ToImmutableList());
-
-            return _resultCreator.Create(damageSkillResult);
-        }
-        
-        
         public ResultEntity Execute(SkillEntity skill)
         {
-            // var actorId = _orderedItems.FirstCharacterId();
-            // var damageSkill = (AbstractDamage)skill.FirstSkillService();
-            // var damageList = new List<DamageValueObject>();
-            // for (var i = 0; i < damageSkill.GetAttackNum(); ++i)
-            // {
-            //     var targetIdList = _target.Get(actorId, skill.AbstractSkill.GetRange());
-            //     foreach (var targetId in targetIdList)
-            //     {
-            //         var damage = new DamageValueObject(
-            //             damageSkill.GetDamageAmount(targetId),
-            //             isHit: damageSkill.IsHit(targetId),
-            //             attacksWeakPoint: damageSkill.AttacksWeakPoint(targetId),
-            //             targetId: targetId,
-            //             number: i);
-            //         damageList.Add(damage);
-            //     }
-            // }
-            //
-            // damageList.Sort((x, y) => x.Number - y.Number);
-            // var damageSkillResult = new DamageSkillResultValueObject(
-            //     actorId,
-            //     skill.SkillCode,
-            //     damageList.ToImmutableList());
-            //
-            // return _resultCreator.Create(damageSkillResult);
-
             throw new NotImplementedException();
         }
     }
