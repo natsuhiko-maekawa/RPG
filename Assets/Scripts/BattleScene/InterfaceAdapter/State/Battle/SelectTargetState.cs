c40493d6ea2ceae921071a8053a85027eecd0636
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-04 11:37:09 +0900

    不要な処理を削除

--- Assets/Scripts/BattleScene/InterfaceAdapter/State/Battle/SelectTargetState.cs
+++ Assets/Scripts/BattleScene/InterfaceAdapter/State/Battle/SelectTargetState.cs
@@ -1,21 +1,9 @@
-﻿using BattleScene.UseCases.View;
-using BattleScene.UseCases.View.GridView;
-
-namespace BattleScene.InterfaceAdapter.State.Battle
+﻿namespace BattleScene.InterfaceAdapter.State.Battle
 {
     public class SelectTargetState : AbstractState
     {
-        private readonly IViewPresenter<GridViewOutputData> _gridView;
-
-        public SelectTargetState(
-            IViewPresenter<GridViewOutputData> gridView)
-        {
-            _gridView = gridView;
-        }
-
         public override void Start()
         {
-            _gridView.Stop();
         }
     }
 }
--- Assets/Scripts/BattleScene/InterfaceAdapter/State/Battle/SelectTargetStateFactory.cs
+++ Assets/Scripts/BattleScene/InterfaceAdapter/State/Battle/SelectTargetStateFactory.cs
@@ -1,22 +1,12 @@
 ﻿using BattleScene.Domain.Code;
-using BattleScene.UseCases.View;
-using BattleScene.UseCases.View.GridView;
 
 namespace BattleScene.InterfaceAdapter.State.Battle
 {
     public class SelectTargetStateFactory
     {
-        private readonly IViewPresenter<GridViewOutputData> _gridView;
-
-        public SelectTargetStateFactory(
-            IViewPresenter<GridViewOutputData> gridView)
-        {
-            _gridView = gridView;
-        }
-        
         public SelectTargetState Create(ActionCode actionCode)
         {
-            return new SelectTargetState(_gridView);
+            return new SelectTargetState();
         }
     }
 }
