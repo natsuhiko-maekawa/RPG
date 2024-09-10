af2516a18f7e1389b6ee86984f922049ccd85001
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-10 10:32:14 +0900

    Windowを非表示にできるよう修正

--- Assets/Scripts/BattleScene/Framework/GameObjects/Window.cs
+++ Assets/Scripts/BattleScene/Framework/GameObjects/Window.cs
@@ -1,27 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace BattleScene.Framework.GameObjects
 {
     public class Window : MonoBehaviour
     {
         private Animator _animator;
+        private Image _window;
         private static readonly int ShowTrigger = Animator.StringToHash("Show");
 
         private void Awake()
         {
             _animator = GetComponent<Animator>();
-            enabled = false;
+            _window = GetComponent<Image>();
+            _window.enabled = false;
         }
 
         public void Show()
         {
-            enabled = true;
+            _window.enabled = true;
             _animator.SetTrigger(ShowTrigger);
         }
 
         public void Hide()
         {
-            enabled = false;
+            _window.enabled = false;
         }
     }
 }
