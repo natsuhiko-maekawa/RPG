a16692ff7eeab7cc0057ffa1606e36354b2b0121
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-11 10:08:08 +0900

    不要な処理を削除

--- Assets/Scripts/BattleScene/UseCases/OldEvent/SlipDamageMessageOldEvent.cs
+++ Assets/Scripts/BattleScene/UseCases/OldEvent/SlipDamageMessageOldEvent.cs
@@ -1,5 +1,3 @@
-using BattleScene.Domain.DomainService;
-using BattleScene.Domain.IRepository;
 using BattleScene.UseCases.OldEvent.Interface;
 using BattleScene.UseCases.OldEvent.Runner;
 
@@ -7,26 +5,13 @@ namespace BattleScene.UseCases.OldEvent
 {
     internal class SlipDamageMessageOldEvent : IOldEvent, IWait
     {
-        private readonly IAilmentRepository _ailmentRepository;
-        private readonly CharactersDomainService _characters;
-        private readonly OrderedItemsDomainService _orderedItems;
-
-        public SlipDamageMessageOldEvent(
-            IAilmentRepository ailmentRepository,
-            CharactersDomainService characters,
-            OrderedItemsDomainService orderedItems)
-        {
-            _ailmentRepository = ailmentRepository;
-            _characters = characters;
-            _orderedItems = orderedItems;
-        }
-
         public EventCode Run()
         {
-            var playerId = _characters.GetPlayerId();
-            var ailmentCode = _orderedItems.FirstAilmentCode();
-            var ailment = _ailmentRepository.Select(playerId, ailmentCode);
-            ailment.AdvanceTurn();
+            // スリップダメージのターンを進める
+            // var playerId = _characters.GetPlayerId();
+            // var ailmentCode = _orderedItems.FirstAilmentCode();
+            // var ailment = _ailmentRepository.Select(playerId, ailmentCode);
+            // ailment.AdvanceTurn();
 
             // メッセージを表示してプレイヤーの立ち絵を更新
             // var slipDamageViewInfo = _slipDamageViewInfoFactory.Create(_orderedItems.FirstSlipDamageCode());
