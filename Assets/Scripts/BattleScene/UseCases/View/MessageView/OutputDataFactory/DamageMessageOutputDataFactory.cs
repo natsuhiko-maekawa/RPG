dfa9a2c317e2b50bd000a63999fd8798088633d8
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-11 09:44:18 +0900

    DamageMessageOutputDataFactoryを修正

--- Assets/Scripts/BattleScene/UseCases/View/MessageView/OutputDataFactory/DamageMessageOutputDataFactory.cs
+++ Assets/Scripts/BattleScene/UseCases/View/MessageView/OutputDataFactory/DamageMessageOutputDataFactory.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Linq;
 using BattleScene.Domain.Code;
 using BattleScene.Domain.DomainService;
+using BattleScene.Domain.Id;
 using BattleScene.UseCases.View.MessageView.OutputData;
+using UnityEngine;
 
 namespace BattleScene.UseCases.View.MessageView.OutputDataFactory
 {
@@ -9,29 +12,45 @@ namespace BattleScene.UseCases.View.MessageView.OutputDataFactory
     {
         private readonly MessageOutputDataFactory _messageOutputDataFactory;
         private readonly OrderedItemsDomainService _orderedItems;
-        private readonly ResultDomainService _result;
+        private readonly BattleLogDomainService _battleLog;
 
         public DamageMessageOutputDataFactory(
             MessageOutputDataFactory messageOutputDataFactory,
             OrderedItemsDomainService orderedItems,
-            ResultDomainService result)
+            BattleLogDomainService battleLog)
         {
             _messageOutputDataFactory = messageOutputDataFactory;
             _orderedItems = orderedItems;
-            _result = result;
+            _battleLog = battleLog;
         }
 
         public MessageOutputData Create()
         {
-            if (_result.LastDamage().AttackList.Any(x => x.AttacksWeakPoint))
-                return _messageOutputDataFactory.Create(MessageCode.WeakPointMessage);
-            if (_result.LastDamage().AttackList
-                .Any(x => !Equals(x.TargetId, _orderedItems.FirstCharacterId()) && x.IsHit))
-                return _messageOutputDataFactory.Create(MessageCode.DamageMessage);
-            if (_result.LastDamage().AttackList
-                .Any(x => Equals(x.TargetId, _orderedItems.FirstCharacterId()) && x.IsHit))
-                return _messageOutputDataFactory.Create(MessageCode.DamageOneselfMessage);
-            return _messageOutputDataFactory.Create(MessageCode.AvoidMessage);
+            var messageCode = GetMessageCode();
+            Debug.Assert(messageCode != MessageCode.NoMessage);
+            return _messageOutputDataFactory.Create(messageCode);
+        }
+
+        private MessageCode GetMessageCode()
+        {
+            if (IsAvoid()) return MessageCode.AvoidMessage;
+            if (DamagesOneself()) return MessageCode.DamageMessage;
+            if (AttacksWeakPoint()) return MessageCode.WeakPointMessage;
+            if (DamagesTarget()) return MessageCode.DamageMessage;
+            return MessageCode.NoMessage;
+        }
+        
+        private bool IsAvoid() => _battleLog.GetLast().AttackList.All(x => !x.IsHit);
+        private bool AttacksWeakPoint() => _battleLog.GetLast().AttackList.Any(x => x.AttacksWeakPoint);
+        private bool DamagesOneself() 
+            => _battleLog.GetLast().AttackList.Any(x => Equals(x.TargetId, GetActor())) && !IsAvoid();
+        
+        private bool DamagesTarget() => !DamagesOneself() && !IsAvoid();
+
+        private CharacterId GetActor()
+        {
+            if (!_orderedItems.First().TryGetCharacterId(out var actorId)) throw new InvalidOperationException();
+            return actorId;
         }
     }
 }
