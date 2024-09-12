17124d2df0ccf563136e37e3f56501d40012e8e8
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-12 14:19:14 +0900

    状態異常メッセージを表示する状態を追加

--- Assets/Scripts/BattleScene/InterfaceAdapter/State/Skill/SlipFailureState.cs
+++ Assets/Scripts/BattleScene/InterfaceAdapter/State/Skill/SlipFailureState.cs
@@ -1,7 +1,29 @@
-﻿namespace BattleScene.InterfaceAdapter.State.Skill
+﻿using BattleScene.Domain.Code;
+using BattleScene.UseCases.View.MessageView.OutputBoundary;
+
+namespace BattleScene.InterfaceAdapter.State.Skill
 {
     public class SlipFailureState : AbstractSkillState
     {
+        private readonly IMessageViewPresenter _messageView;
+        private readonly SkillEndState _skillEndState;
+
+        public SlipFailureState(
+            IMessageViewPresenter messageView,
+            SkillEndState skillEndState)
+        {
+            _messageView = messageView;
+            _skillEndState = skillEndState;
+        }
+
+        public override void Start()
+        {
+            _messageView.Start(MessageCode.FailAilmentMessage);
+        }
         
+        public override void Select()
+        {
+            SkillContext.TransitionTo(_skillEndState);
+        }
     }
 }
--- Assets/Scripts/BattleScene/InterfaceAdapter/State/Skill/SlipMessageState.cs
+++ Assets/Scripts/BattleScene/InterfaceAdapter/State/Skill/SlipMessageState.cs
@@ -1,7 +1,29 @@
-﻿namespace BattleScene.InterfaceAdapter.State.Skill
+﻿using BattleScene.Domain.Code;
+using BattleScene.UseCases.View.MessageView.OutputBoundary;
+
+namespace BattleScene.InterfaceAdapter.State.Skill
 {
     public class SlipMessageState : AbstractSkillState
     {
+        private readonly IMessageViewPresenter _messageView;
+        private readonly SkillEndState _skillEndState;
+
+        public SlipMessageState(
+            IMessageViewPresenter messageView,
+            SkillEndState skillEndState)
+        {
+            _messageView = messageView;
+            _skillEndState = skillEndState;
+        }
+
+        public override void Start()
+        {
+            _messageView.Start(MessageCode.AilmentMessage);
+        }
         
+        public override void Select()
+        {
+            SkillContext.TransitionTo(_skillEndState);
+        }
     }
 }
