c6aca68020ff520e05c35f2e7c6727d0da10ff6c
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-12 09:50:00 +0900

    不要なフィールドを削除

--- Assets/Scripts/BattleScene/Domain/Entity/AilmentEntity.cs
+++ Assets/Scripts/BattleScene/Domain/Entity/AilmentEntity.cs
@@ -1,13 +1,10 @@
 ﻿using BattleScene.Domain.Code;
 using BattleScene.Domain.Id;
-using BattleScene.Domain.ValueObject;
 
 namespace BattleScene.Domain.Entity
 {
     public class AilmentEntity : BaseEntity<AilmentEntity, (CharacterId, AilmentCode)>
     {
-        private TurnValueObject _turn;
-        
         public AilmentEntity(
             CharacterId characterId,
             AilmentCode ailmentCode,
@@ -25,14 +22,6 @@ namespace BattleScene.Domain.Entity
         public AilmentCode AilmentCode { get; }
         public int Turn { get; private set; }
         public bool IsSelfRecovery { get; }
-
-        public void AdvanceTurn()
-        {
-            if (!IsSelfRecovery) return;
-            if (Turn <= 0) return;
-            Turn--;
-        }
-
         public bool TurnIsEnd
         {
             get
@@ -40,6 +29,13 @@ namespace BattleScene.Domain.Entity
                 if (!IsSelfRecovery) return false;
                 return Turn < 0;
             }
+        }
+
+        public void AdvanceTurn()
+        {
+            if (!IsSelfRecovery) return;
+            if (Turn <= 0) return;
+            Turn--;
         }
     }
 }
