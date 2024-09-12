b919b0f7b4e6a9f53ee82f56fcbba473702e5277
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-12 23:04:54 +0900

    SlipDamageEntityを修正

--- Assets/Scripts/BattleScene/Domain/Entity/SlipDamageEntity.cs
+++ Assets/Scripts/BattleScene/Domain/Entity/SlipDamageEntity.cs
@@ -1,47 +1,29 @@
-﻿using System;
-using BattleScene.Domain.Code;
-using BattleScene.Domain.ValueObject;
+﻿using BattleScene.Domain.Code;
 
 namespace BattleScene.Domain.Entity
 {
     public class SlipDamageEntity : BaseEntity<SlipDamageEntity, SlipDamageCode>
     {
-        private readonly int _defaultTurn;
-        private TurnValueObject _turn;
-
         public SlipDamageEntity(
-            SlipDamageCode id,
-            SlipDamageEntityDto dto,
-            TurnValueObject turn)
+            SlipDamageCode slipDamageCode,
+            int turn)
         {
-            Id = id;
-            DamageRate = dto.DamageRate;
-            EnemyIntelligence = dto.EnemyIntelligence;
-            PlayerIntelligence = dto.PlayerIntelligence;
-            _turn = turn;
-            if (_turn.Get() == null) throw new NullReferenceException();
-            _defaultTurn = _turn.Get().GetValueOrDefault();
+            Id = slipDamageCode;
+            Turn = turn;
+            DefaultTurn = turn;
         }
         
         public override SlipDamageCode Id { get; }
+        public int Turn { get; private set; }
+        private int DefaultTurn { get; }
         public float DamageRate { get; }
         public int EnemyIntelligence { get; }
         public int PlayerIntelligence { get; }
 
-        public int? GetTurn()
-        {
-            return _turn.Get();
-        }
-
         public void AdvanceTurn()
         {
-            _turn.Advance();
-            if (_turn.IsEnd()) _turn = new TurnValueObject(_defaultTurn);
+            Turn--;
+            if (Turn < 0) Turn = DefaultTurn;
         }
     }
-
-    public record SlipDamageEntityDto(
-        float DamageRate,
-        int PlayerIntelligence,
-        int EnemyIntelligence);
 }
