578d73e4687ec10e624f5ca9fa135597c40e4683
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-16 08:59:22 +0900

    UseCases層にDIコンテナを追加

---
+++ Assets/Scripts/BattleScene/UseCases/BattleSceneLifetimeScope.cs
@@ -0,0 +1,15 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace BattleScene.UseCases
+{
+    /// <summary>
+    /// InterfaceAdapter層とUseCases層を統合するまでの仮のコンテナ
+    /// </summary>
+    public class BattleSceneLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
