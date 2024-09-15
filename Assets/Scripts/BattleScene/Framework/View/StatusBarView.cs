866e2a563419d7080548f4f31254e611afc33fb8
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-15 10:11:58 +0900

    不要なコードを削除

--- Assets/Scripts/BattleScene/Framework/View/StatusBarView.cs
+++ Assets/Scripts/BattleScene/Framework/View/StatusBarView.cs
@@ -8,8 +8,6 @@ namespace BattleScene.Framework.View
 {
     public class StatusBarView : MonoBehaviour
     {
-        [SerializeField] private Image barFrontImage;
-        [SerializeField] private Text barText;
         private Image _barFrontImage;
         private Text _barText;
         private StatusBarMeter _statusBarMeter;
@@ -17,18 +15,12 @@ namespace BattleScene.Framework.View
 
         private void Awake()
         {
-            // _barFrontImage = Instantiate(barFrontImage, transform);
-            // _barText = Instantiate(barText, transform);
             _statusBarMeter = GetComponentInChildren<StatusBarMeter>();
             _statusBarText = GetComponentInChildren<StatusBarText>();
         }
 
         public Task StartAnimation(StatusBarViewDto dto)
         {
-            // _barText.text = dto.CurrentPoint + "/" + dto.MaxPoint;
-            // var hpRate = dto.MaxPoint < dto.CurrentPoint ? 1.0f : dto.CurrentPoint / (float)dto.MaxPoint;
-            // _barFrontImage.rectTransform.localScale = new Vector3(hpRate, 1.0f, 1.0f);
-            
             _statusBarText.Set(dto.CurrentPoint, dto.MaxPoint);
             _statusBarMeter.Set(dto.CurrentPoint, dto.MaxPoint);
             
