61c7d1b84b5585ae6c99044e51291328afbb0a99
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-15 09:14:58 +0900

    不要な処理を削除

--- Assets/Scripts/BattleScene/Framework/View/EnemiesView.cs
+++ Assets/Scripts/BattleScene/Framework/View/EnemiesView.cs
@@ -1,40 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using BattleScene.Framework.GameObjects;
 using BattleScene.InterfaceAdapter.Interface;
 using BattleScene.InterfaceAdapter.Presenter.AilmentsView;
 using BattleScene.InterfaceAdapter.Presenter.CharacterVibesView;
-using BattleScene.InterfaceAdapter.Presenter.DigitView;
-using BattleScene.InterfaceAdapter.Presenter.EnemyView;
 using BattleScene.InterfaceAdapter.Presenter.FrameView;
-using BattleScene.InterfaceAdapter.Presenter.StatusBarView;
 using UnityEngine;
 
 namespace BattleScene.Framework.View
 {
     public class EnemiesView : MonoBehaviour, IEnemiesView, IEnumerable<EnemyView>
     {
-        [SerializeField] private GameObject enemyView;
         [SerializeField] private int maxCacheSize = 4;
-        [SerializeField] private float enemyIntervalPx = 200;
         private EnemyView _enemyView;
-        private readonly List<EnemyView> _enemyViewList = new();
         private EnemyGrid _enemyViewGrid;
 
         private void Awake()
         {
-            // _enemyView = GetComponentInChildren<EnemyView>();
-            // for (var i = 0; i < maxCacheSize; ++i)
-            // {
-            //     var enemyViewScript = Instantiate(_enemyView, transform);
-            //     Debug.Assert(enemyViewScript != null);
-            //     enemyViewScript.SetActive(false);
-            //     _enemyViewList.Add(enemyViewScript);
-            // }
-
             _enemyViewGrid = GetComponent<EnemyGrid>();
             _enemyViewGrid.Initialize();
             _enemyViewGrid.SetItem(maxCacheSize);
@@ -42,18 +26,6 @@ namespace BattleScene.Framework.View
 
         [Obsolete]
         public Task StartEnemyAilmentsView(EnemyAilmentsViewDto dto)
-        {
-            return Task.CompletedTask;
-        }
-        
-        [Obsolete]
-        public Task StartEnemyDigitView(EnemyDigitViewDto dto)
-        {
-            return Task.CompletedTask;
-        }
-
-        [Obsolete]
-        public Task StartEnemyHpBarView(EnemyHpBarViewDto dto)
         {
             return Task.CompletedTask;
         }
@@ -77,32 +49,9 @@ namespace BattleScene.Framework.View
 
         public IEnemyView this[int i] => _enemyViewGrid[i];
 
-        public async Task StartEnemyView(EnemyViewDto dto)
-        {
-            // foreach (var (enemyViewInstance, index) in _enemyViewList.Select((x, i) => (x, i)))
-            // {
-            //     if (!dto.EnemyDtoList.Select(x => x.EnemyNumber).Contains(index)) continue;
-            //     
-            //     enemyViewInstance.SetActive(true);
-            //     var enemyImagePath = dto.EnemyDtoList.First(y => y.EnemyNumber == index).EnemyImagePath;
-            //     var sprite = await _spriteFlyweight.Get(enemyImagePath);
-            //     enemyViewInstance.GetComponent<Image>().sprite = sprite;
-            //     var vector3 = new Vector3(GetX(dto.EnemyCount, index), 0.0f);
-            //     enemyViewInstance.transform.localPosition += vector3;
-            // }
-        }
-
-        private float GetX(int enemyCount, int enemyPosition)
-        {
-            return Enumerable.Range(0, enemyCount)
-                .Select(x => (enemyCount - 1) * -enemyIntervalPx / 2 + x * enemyIntervalPx)
-                .Where((_, i) => i == enemyPosition)
-                .First();
-        }
-
         public IEnumerator<EnemyView> GetEnumerator()
         {
-            return _enemyViewList.GetEnumerator();
+            return _enemyViewGrid.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
