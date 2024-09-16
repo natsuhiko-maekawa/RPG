c858aaabf3d95a946422e451cf1e8670f550d375
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-16 14:16:19 +0900

    警告を解消

--- Assets/Scripts/BattleScene/UseCases/OldEvent/EnemySuicideOldEvent.cs
+++ Assets/Scripts/BattleScene/UseCases/OldEvent/EnemySuicideOldEvent.cs
@@ -3,8 +3,6 @@ using BattleScene.UseCases.OldEvent.Interface;
 using BattleScene.UseCases.OldEvent.Runner;
 using BattleScene.UseCases.View.AilmentView.OutputBoundary;
 using BattleScene.UseCases.View.AilmentView.OutputDataFactory;
-using BattleScene.UseCases.View.EnemyView.OutputBoundary;
-using BattleScene.UseCases.View.EnemyView.OutputDataFactory;
 using BattleScene.UseCases.View.MessageView.OutputBoundary;
 using BattleScene.UseCases.View.MessageView.OutputDataFactory;
 using static BattleScene.UseCases.OldEvent.Runner.EventCode;
@@ -16,8 +14,6 @@ namespace BattleScene.UseCases.OldEvent
     {
         private readonly AilmentOutputDataFactory _ailmentOutputDataFactory;
         private readonly IAilmentViewPresenter _ailmentView;
-        private readonly EnemyOutputDataFactory _enemyOutputDataFactory;
-        private readonly IEnemyViewPresenter _enemyView;
         private readonly MessageOutputDataFactory _messageOutputDataFactory;
         private readonly IMessageViewPresenter _messageView;
         private readonly OrderedItemsDomainService _orderedItems;
@@ -25,16 +21,12 @@ namespace BattleScene.UseCases.OldEvent
         public EnemySuicideOldEvent(
             AilmentOutputDataFactory ailmentOutputDataFactory,
             IAilmentViewPresenter ailmentView,
-            EnemyOutputDataFactory enemyOutputDataFactory,
-            IEnemyViewPresenter enemyView,
             MessageOutputDataFactory messageOutputDataFactory,
             IMessageViewPresenter messageView,
             OrderedItemsDomainService orderedItems)
         {
             _ailmentOutputDataFactory = ailmentOutputDataFactory;
             _ailmentView = ailmentView;
-            _enemyOutputDataFactory = enemyOutputDataFactory;
-            _enemyView = enemyView;
             _messageOutputDataFactory = messageOutputDataFactory;
             _messageView = messageView;
             _orderedItems = orderedItems;
@@ -50,8 +42,9 @@ namespace BattleScene.UseCases.OldEvent
 
             var ailmentOutputData = _ailmentOutputDataFactory.Create(characterId);
             _ailmentView.Start(ailmentOutputData);
-            var enemyOutputData = _enemyOutputDataFactory.Create();
-            _enemyView.Start(enemyOutputData);
+            // 敵の画像の表示を更新
+            // var enemyOutputData = _enemyOutputDataFactory.Create();
+            // _enemyView.Start(enemyOutputData);
             var messageOutputData = _messageOutputDataFactory.Create(EnemySuicideMessage);
             _messageView.Start(messageOutputData);
 
