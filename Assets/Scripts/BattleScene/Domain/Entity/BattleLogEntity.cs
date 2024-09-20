5dc0d25d31df00fae6d53038641ce385ef0f008d
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-20 11:07:01 +0900

    不要なusingを削除

--- Assets/Scripts/BattleScene/Domain/Entity/BattleLogEntity.cs
+++ Assets/Scripts/BattleScene/Domain/Entity/BattleLogEntity.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using BattleScene.Domain.Code;
 using BattleScene.Domain.Id;
 using BattleScene.Domain.ValueObject;
