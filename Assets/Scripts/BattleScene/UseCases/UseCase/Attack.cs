7f547bf9c3d5a356d394e5ed2f38a4005a6796c3
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-21 14:41:34 +0900

    不要な処理を削除

--- Assets/Scripts/BattleScene/UseCases/UseCase/Attack.cs
+++ Assets/Scripts/BattleScene/UseCases/UseCase/Attack.cs
@@ -5,8 +5,6 @@ using BattleScene.Domain.IRepository;
 using BattleScene.UseCases.OldEvent.Runner;
 using BattleScene.UseCases.View.FrameView.OutputBoundary;
 using BattleScene.UseCases.View.FrameView.OutputDataFactory;
-using BattleScene.UseCases.View.TechnicalPointBarView.OutputBoundary;
-using BattleScene.UseCases.View.TechnicalPointBarView.OutputDaraFactory;
 
 namespace BattleScene.UseCases.UseCase
 {
@@ -16,23 +14,17 @@ namespace BattleScene.UseCases.UseCase
         private readonly IFrameViewPresenter _frameView;
         private readonly OrderedItemsDomainService _orderedItems;
         private readonly TargetFrameOutputDataFactory _targetFrameOutputDataFactory;
-        private readonly TechnicalPointBarOutputDataFactory _technicalPointBarOutputDataFactory;
-        private readonly ITechnicalPointBarViewPresenter _technicalPointBarView;
 
         public Attack(
             IRepository<CharacterEntity, CharacterId> characterRepository,
             IFrameViewPresenter frameView,
             OrderedItemsDomainService orderedItems,
-            TargetFrameOutputDataFactory targetFrameOutputDataFactory,
-            TechnicalPointBarOutputDataFactory technicalPointBarOutputDataFactory,
-            ITechnicalPointBarViewPresenter technicalPointBarView)
+            TargetFrameOutputDataFactory targetFrameOutputDataFactory)
         {
             _characterRepository = characterRepository;
             _frameView = frameView;
             _orderedItems = orderedItems;
             _targetFrameOutputDataFactory = targetFrameOutputDataFactory;
-            _technicalPointBarOutputDataFactory = technicalPointBarOutputDataFactory;
-            _technicalPointBarView = technicalPointBarView;
         }
 
         public void Execute()
@@ -51,8 +43,9 @@ namespace BattleScene.UseCases.UseCase
                 // var playerImageOutputData = _playerAttackPlayerImageOutputDataFactory.Create();
                 // _playerImageView.Start(playerImageOutputData);
 
-                var technicalPointBarOutputData = _technicalPointBarOutputDataFactory.Create();
-                _technicalPointBarView.Start(technicalPointBarOutputData);
+                // TPバーの表示を更新
+                // var technicalPointBarOutputData = _technicalPointBarOutputDataFactory.Create();
+                // _technicalPointBarView.Start(technicalPointBarOutputData);
 
                 // 通常攻撃の場合、通常攻撃のメッセージを表示
                 // スキルの場合、スキルのメッセージを表示
