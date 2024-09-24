58e8d4d7cdcb00d763ac350c23f6ea29617c0e3b
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-09-24 14:35:49 +0900

    不要な処理を削除

--- Assets/Scripts/BattleScene/Framework/View/PlayerStatusView.cs
+++ Assets/Scripts/BattleScene/Framework/View/PlayerStatusView.cs
@@ -12,52 +12,13 @@ namespace BattleScene.Framework.View
         private PlayerAilmentStatus[] _playerAilmentStatusArray;
         private PlayerBodyPartStatus[] _playerBodyPartStatusArray;
         private PlayerBuffView _playerBuffView;
-        private PlayerDestroyedPartView _playerDestroyedPartView;
         private Text[] _textGroup;
 
         private void Awake()
         {
-            // var groups = GetComponentsInChildren<GridLayoutGroup>();
-            // _textGroup = groups[0].GetComponentsInChildren<Text>();
-            // _iconGroup = groups[1].GetComponentsInChildren<Image>();
             _playerAilmentStatusArray = GetComponentsInChildren<PlayerAilmentStatus>();
             _playerBodyPartStatusArray = GetComponentsInChildren<PlayerBodyPartStatus>();
-            _playerDestroyedPartView = GetComponent<PlayerDestroyedPartView>();
             _playerBuffView = GetComponent<PlayerBuffView>();
-
-            // foreach (var icon in _iconGroup) icon.enabled = false;
-
-            // var ailmentsViewTextAndIconList = Enumerable.Range(1, 15)
-            //     .Select(x => new TextAndIcon
-            //     {
-            //         text = _textGroup[x],
-            //         icon = _iconGroup[x]
-            //     })
-            //     .ToList();
-
-            // foreach (var playerAilmentStatus in _playerAilmentStatusArray)
-            // {
-            //     playerAilmentStatus.Inactivate();
-            // }
-
-            // var destroyedPartViewTextAndIconList = Enumerable.Range(17, 3)
-            //     .Select(x => new TextAndIcon
-            //     {
-            //         text = _textGroup[x],
-            //         icon = _iconGroup[x]
-            //     })
-            //     .ToList();
-            // _playerDestroyedPartView.Initialize(destroyedPartViewTextAndIconList);
-
-            // var indexList = new List<int> { 21, 22, 24, 25, 26 };
-            // var buffViewTextAndIconList = indexList
-            //     .Select(x => new TextAndIcon
-            //     {
-            //         text = _textGroup[x],
-            //         icon = _iconGroup[x]
-            //     })
-            //     .ToList();
-            // _playerBuffView.Initialize(buffViewTextAndIconList);
         }
 
         public void StartPlayerAilmentsView(AilmentViewModel ailment)
