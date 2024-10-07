60359cc00ee43e9a07eb38eb3781b5c9ca0d5cf3
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-10-07 16:13:10 +0900

    フィールドをDictionaryに修正

--- Assets/Scripts/BattleScene/DataAccess/Repository/Repository.cs
+++ Assets/Scripts/BattleScene/DataAccess/Repository/Repository.cs
@@ -8,52 +8,47 @@ namespace BattleScene.DataAccess.Repository
     public partial class Repository<TEntity, TId> : IRepository<TEntity, TId>, ISerializable
         where TEntity : BaseEntity<TId>
     {
-        private readonly HashSet<TEntity> _entitySet = new();
-
+        private readonly Dictionary<TId, TEntity> _entityDictionary = new();
+        
         public TEntity Select(TId id)
         {
-            return _entitySet
-                .FirstOrDefault(x => Equals(x.Id, id));
+            _entityDictionary.TryGetValue(id, out var entity);
+            return entity;
         }
 
-        public IReadOnlyList<TEntity> Select()
-        {
-            return _entitySet.ToList();
-        }
+        public IReadOnlyList<TEntity> Select() => _entityDictionary.Values.ToList();
 
         public void Update(TEntity entity)
         {
             if (entity == null) return;
             Observe(entity);
-            _entitySet.Add(entity);
+            
+            // TODO: TryAddをAddに修正すること
+            if (_entityDictionary.TryAdd(entity.Id, entity)) return;
+            _entityDictionary.Remove(entity.Id);
+            _entityDictionary.Add(entity.Id, entity);
         }
 
         partial void Observe(TEntity entity);
 
         public void Update(IReadOnlyList<TEntity> entityList)
         {
-            foreach (var entity in entityList)
-            {
-                Update(entity);
-            }
+            foreach (var entity in entityList) Update(entity);
         }
 
         public void Delete()
         {
-            _entitySet.Clear();
+            _entityDictionary.Clear();
         }
         
         public void Delete(TId id)
         {
-            _entitySet.RemoveWhere(x => Equals(x.Id, id));
+            _entityDictionary.Remove(id);
         }
 
         public void Delete(IReadOnlyList<TId> idList)
         {
-            foreach (var id in idList)
-            {
-                Delete(id);
-            }
+            foreach (var id in idList) Delete(id);
         }
     }
 }
