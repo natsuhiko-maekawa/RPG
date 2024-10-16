49555e88d2516daa0690f567c8265cf35dde4038
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-10-16 12:07:31 +0900

    不要なオーバーライドを削除

--- Assets/Scripts/BattleScene/Domain/Entity/BaseEntity.cs
+++ Assets/Scripts/BattleScene/Domain/Entity/BaseEntity.cs
@@ -3,18 +3,5 @@
     public abstract class BaseEntity<TId>
     {
         public abstract TId Id { get; }
-
-        // TODO: 以下の二つのオーバーライドを削除すること
-        public override bool Equals(object obj)
-        {
-            if (obj == null || GetType() != obj.GetType()) return false;
-            var entity = (BaseEntity<TId>)obj;
-            return Equals(Id, entity.Id);
-        }
-
-        public override int GetHashCode()
-        {
-            return Id.GetHashCode();
-        }
     }
 }
