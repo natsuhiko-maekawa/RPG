24de7851bd29528e351b20aff3beffa246f2f6bc
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-10-18 10:44:14 +0900

    不要なusingを削除

--- Assets/Scripts/LoadingScene/InterfaceAdapter/Repository/TipsRepository.cs
+++ Assets/Scripts/LoadingScene/InterfaceAdapter/Repository/TipsRepository.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using LoadingScene.InterfaceAdapter.Repository.IScriptableObject;
 using LoadingScene.UseCase.IRepository;
