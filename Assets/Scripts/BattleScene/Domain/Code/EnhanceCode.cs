1f07b8bd1863f7668b7d7f5623f561b3f53a4e45
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-10-22 08:49:06 +0900

    EnhanceEntityを追加

---
+++ Assets/Scripts/BattleScene/Domain/Code/EnhanceCode.cs
@@ -0,0 +1,8 @@
+namespace BattleScene.Domain.Code
+{
+    public enum EnhanceCode
+    {
+        Defence,
+        Utsusemi
+    }
+}
---
+++ Assets/Scripts/BattleScene/Domain/Entity/EnhanceEntity.cs
@@ -0,0 +1,47 @@
+using BattleScene.Domain.Code;
+using BattleScene.Domain.Id;
+
+namespace BattleScene.Domain.Entity
+{
+    public partial class EnhanceEntity : BaseEntity<(CharacterId, EnhanceCode)>
+    {
+        public EnhanceEntity(
+            CharacterId characterId,
+            EnhanceCode enhanceCode,
+            int turn,
+            LifetimeCode lifetimeCode)
+        {
+            CharacterId = characterId;
+            EnhanceCode = enhanceCode;
+            Turn = turn;
+            LifetimeCode = lifetimeCode;
+            Effects = true;
+        }
+        
+        public override (CharacterId, EnhanceCode) Id => (CharacterId, EnhanceCode);
+        public CharacterId CharacterId { get; }
+        public EnhanceCode EnhanceCode { get; }
+        public int Turn { get; private set; }
+        public LifetimeCode LifetimeCode { get; }
+        
+        private bool _effects;
+
+        public bool Effects
+        {
+            get => _effects;
+            set
+            {
+                _effects = value;
+                EffectsOnChange(value);
+            }
+        }
+
+        partial void EffectsOnChange(bool value);
+        
+        public void AdvanceTurn()
+        {
+            Turn--;
+            if (Turn < 0 && Effects) Effects = false;
+        }
+    }
+}
