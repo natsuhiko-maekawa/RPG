a2072438192392a024bf6aa5bb208de409b1abe4
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-10-22 09:07:35 +0900

    EnhanceListを追加

--- Assets/Scripts/BattleScene/InterfaceAdapter/Skill/BaseClass/BaseSkill.cs
+++ Assets/Scripts/BattleScene/InterfaceAdapter/Skill/BaseClass/BaseSkill.cs
@@ -15,17 +15,13 @@ namespace BattleScene.InterfaceAdapter.Skill.BaseClass
         public abstract MessageCode AttackMessageCode { get; }
 
         public virtual IReadOnlyList<BaseAilment> AilmentList { get; } = MyList<BaseAilment>.Empty;
-
-        public virtual IReadOnlyList<BaseSlip> SlipDamageList { get; } =
-            MyList<BaseSlip>.Empty;
-
-        public virtual IReadOnlyList<BaseDestroy> DestroyList { get; } =
-            MyList<BaseDestroy>.Empty;
-
-        public virtual IReadOnlyList<BaseDamage> DamageList { get; } = MyList<BaseDamage>.Empty;
         public virtual IReadOnlyList<BaseBuff> BuffList { get; } = MyList<BaseBuff>.Empty;
         public virtual IReadOnlyList<BaseCure> CureList { get; } = MyList<BaseCure>.Empty;
+        public virtual IReadOnlyList<BaseDamage> DamageList { get; } = MyList<BaseDamage>.Empty;
+        public virtual IReadOnlyList<BaseDestroy> DestroyList { get; } = MyList<BaseDestroy>.Empty;
+        public virtual IReadOnlyList<BaseEnhance> EnhanceList { get; } = MyList<BaseEnhance>.Empty;
         public virtual IReadOnlyList<BaseReset> ResetList { get; } = MyList<BaseReset>.Empty;
         public virtual IReadOnlyList<BaseRestore> RestoreList { get; } = MyList<BaseRestore>.Empty;
+        public virtual IReadOnlyList<BaseSlip> SlipList { get; } = MyList<BaseSlip>.Empty;
     }
 }
