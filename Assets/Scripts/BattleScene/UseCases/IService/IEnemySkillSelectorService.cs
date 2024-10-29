46791db46d05e9c1fd9a3528162d72fc32a7a538
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-10-29 09:36:03 +0900

    不要なメソッドを削除

--- Assets/Scripts/BattleScene/UseCases/IService/IEnemySkillSelectorService.cs
+++ Assets/Scripts/BattleScene/UseCases/IService/IEnemySkillSelectorService.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using BattleScene.Domain.Code;
-using BattleScene.Domain.Id;
+﻿using BattleScene.Domain.Id;
 using BattleScene.Domain.ValueObject;
 
 namespace BattleScene.UseCases.IService
 {
     public interface IEnemySkillSelectorService
     {
-        [Obsolete]
-        public SkillCode Select();
         public SkillValueObject Select(CharacterId actorId);
     }
 }
--- Assets/Scripts/BattleScene/UseCases/Service/EnemySkillSelectorService.cs
+++ Assets/Scripts/BattleScene/UseCases/Service/EnemySkillSelectorService.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using BattleScene.Domain.Code;
+﻿using BattleScene.Domain.Code;
 using BattleScene.Domain.DataAccess;
-using BattleScene.Domain.DomainService;
 using BattleScene.Domain.Entity;
 using BattleScene.Domain.Id;
 using BattleScene.Domain.ValueObject;
@@ -13,32 +11,19 @@ namespace BattleScene.UseCases.Service
     {
         private readonly ICollection<CharacterEntity, CharacterId> _characterCollection;
         private readonly IFactory<CharacterPropertyValueObject, CharacterTypeCode> _characterPropertyFactory;
-        private readonly OrderedItemsDomainService _orderItems;
         private readonly IMyRandomService _myRandom;
         private readonly IFactory<SkillValueObject, SkillCode> _skillFactory;
 
         public EnemySkillSelectorService(
             ICollection<CharacterEntity, CharacterId> characterCollection,
             IFactory<CharacterPropertyValueObject, CharacterTypeCode> characterPropertyFactory,
-            OrderedItemsDomainService orderItems,
             IMyRandomService myRandom,
             IFactory<SkillValueObject, SkillCode> skillFactory)
         {
             _characterCollection = characterCollection;
             _characterPropertyFactory = characterPropertyFactory;
-            _orderItems = orderItems;
             _myRandom = myRandom;
             _skillFactory = skillFactory;
-        }
-
-        [Obsolete]
-        public SkillCode Select()
-        {
-            // TODO: 敵がスキルを選択する際、ランダムに選択する仮のアルゴリズムを実装している
-            _orderItems.First().TryGetCharacterId(out var characterId);
-            var characterTypeCode = _characterCollection.Get(characterId).CharacterTypeCode;
-            var skillCodeList = _characterPropertyFactory.Create(characterTypeCode).SkillCodeList;
-            return _myRandom.Choice(skillCodeList);
         }
 
         public SkillValueObject Select(CharacterId actorId)
