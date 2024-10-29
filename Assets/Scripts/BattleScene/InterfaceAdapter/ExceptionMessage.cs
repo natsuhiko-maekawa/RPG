c293670abe6a6352e0b2b11b6941c7a21dace3a7
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-10-29 10:49:37 +0900

    ExceptionMessageクラスを追加

---
+++ Assets/Scripts/BattleScene/InterfaceAdapter/ExceptionMessage.cs
@@ -0,0 +1,7 @@
+namespace BattleScene.InterfaceAdapter
+{
+    public static class ExceptionMessage
+    {
+        public const string ContextSkillIsNull = "Context.Skill expected any value but was null.";
+    }
+}
