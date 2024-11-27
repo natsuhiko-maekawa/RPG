c3b63e005d447ed1bc84af635b3407bcf33f8bd4
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-11-27 13:39:07 +0900

    DigitGameObjectを作成

---
+++ Assets/Scripts/BattleScene/Framework/GameObjects/Digit.cs
@@ -0,0 +1,69 @@
+using System;
+using BattleScene.Framework.ViewModel;
+using Cysharp.Text;
+using TMPro;
+using UnityEngine;
+
+namespace BattleScene.Framework.GameObjects
+{
+    public class Digit : MonoBehaviour
+    {
+        public float x;
+        [SerializeField] private string avoidMessage = "AVOID!";
+        [SerializeField] private VertexGradient damageColor = new(Color.yellow, Color.yellow, Color.red, Color.red);
+        [SerializeField] private VertexGradient cureColor = new(Color.green, Color.green, Color.cyan, Color.cyan);
+        [SerializeField] private VertexGradient restoreColor = new(Color.cyan, Color.cyan, Color.blue, Color.blue);
+        private TMP_Text _tmpText;
+        private Animator _animator;
+        private Vector3 _originalPosition;
+        private static readonly int ShowTrigger = Animator.StringToHash("Show");
+
+        private void Awake()
+        {
+            _tmpText = GetComponent<TMP_Text>();
+            _tmpText.enabled = false;
+            _animator = GetComponent<Animator>();
+            enabled = false;
+        }
+
+        public void SetAvoid()
+        {
+            _tmpText.SetText(avoidMessage);
+            _tmpText.enableVertexGradient = false;
+        }
+
+        public void SetDigit(int value, DigitType digitType)
+        {
+            using (var stringBuilder = ZString.CreateStringBuilder())
+            {
+                stringBuilder.Append(value);
+                _tmpText.SetText(stringBuilder);
+            }
+
+            _tmpText.enableVertexGradient = true;
+            _tmpText.colorGradient = digitType switch
+            {
+                DigitType.Damage => damageColor,
+                DigitType.Cure => cureColor,
+                DigitType.Restore => restoreColor,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        private void OnEnable()
+        {
+            _animator.SetTrigger(ShowTrigger);
+            _tmpText.enabled = true;
+        }
+
+        private void Update()
+        {
+            transform.localPosition = _originalPosition + new Vector3(x, 0, 0);
+        }
+
+        public void OnDisable()
+        {
+            _tmpText.enabled = false;
+        }
+    }
+}
--- Assets/Scripts/BattleScene/Framework/View/DigitView.cs
+++ Assets/Scripts/BattleScene/Framework/View/DigitView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BattleScene.Framework.GameObjects;
 using BattleScene.Framework.ViewModel;
 using TMPro;
 using UnityEngine;
@@ -20,31 +21,29 @@ namespace BattleScene.Framework.View
         [SerializeField] private float alphaRate = 0.0f;
         [SerializeField] private int waitTime = 70;
         [SerializeField] private string avoidText = "avoid";
-        [SerializeField] private VertexGradient damageColor = new(Color.yellow, Color.yellow, Color.red, Color.red);
-        [SerializeField] private VertexGradient cureColor = new(Color.green, Color.green, Color.cyan, Color.cyan);
-        [SerializeField] private VertexGradient restoreColor = new(Color.cyan, Color.cyan, Color.blue, Color.blue);
+
         private const int PoolSize = 10;
         // [SerializeField] private TextMeshProUGUI damageText;
         [SerializeField] private bool isPlayer;
-        private TextMeshProUGUI _text;
-        private readonly TextMeshProUGUI[] _textPool = new TextMeshProUGUI[10];
+        private Digit _digit;
+        private readonly Digit[] _digitPool = new Digit[10];
 
         private void Awake()
         {
-            for (var i = 0; i < _textPool.Length; ++i)
+            for (var i = 0; i < _digitPool.Length; ++i)
             {
-                if (_text is null)
+                if (_digit is null)
                 {
-                    _text = GetComponentInChildren<TextMeshProUGUI>();
-                    _textPool[i] = _text;
+                    _digit = GetComponentInChildren<Digit>();
+                    _digitPool[i] = _digit;
                 }
                 else
                 {
-                    var text = Instantiate(_text, transform);
-                    _textPool[i] = text;
+                    var text = Instantiate(_digit, transform);
+                    _digitPool[i] = text;
                 }
 
-                _textPool[i].enabled = false;
+                _digitPool[i].enabled = false;
             }
         }
 
@@ -65,43 +64,34 @@ namespace BattleScene.Framework.View
             await Task.WhenAll(animationList);
         }
 
-        private async Task StartDigitAnimationAsync(DigitViewModel digit)
+        private async Task StartDigitAnimationAsync(DigitViewModel model)
         {
-            var text = _textPool.First(x => !x.enabled);
+            var digit = _digitPool.First(x => !x.enabled);
 
-            if (digit.IsAvoid)
+            if (model.IsAvoid)
             {
-                text.text = avoidText;
-                text.enableVertexGradient = false;
+                digit.SetAvoid();
             }
             else
             {
-                text.text = digit.Digit.ToString();
-                text.enableVertexGradient = true;
-                text.colorGradient = digit.DigitType switch
-                {
-                    DigitType.Damage => damageColor,
-                    DigitType.Cure => cureColor,
-                    DigitType.Restore => restoreColor,
-                    _ => throw new ArgumentOutOfRangeException()
-                };
+                digit.SetDigit(model.Digit, model.DigitType);
             }
 
-            text.enabled = true;
+            digit.enabled = true;
             var rand = isPlayer ? Mathf.Floor(Random.Range(-randomRange, randomRange) / 10) * 10.0f : 0;
             for (var i = 0; i < frame; ++i)
             {
                 var sin = Mathf.Sin(i / (float)frame * 90 * Mathf.Deg2Rad);
-                text.transform.localPosition = new Vector3(rand, moveRange * sin + rand, 0);
+                digit.transform.localPosition = new Vector3(rand, moveRange * sin + rand, 0);
 
-                var a = 1.0f - alphaRate * sin;
-                a = Mathf.Max(a, 0.0f);
-                text.color = new Color(text.color.r, text.color.g, text.color.b, a);
+                // var a = 1.0f - alphaRate * sin;
+                // a = Mathf.Max(a, 0.0f);
+                // digit.color = new Color(digit.color.r, digit.color.g, digit.color.b, a);
 
                 await Task.Delay(WaitTime);
             }
 
-            text.enabled = false;
+            digit.enabled = false;
         }
     }
 }
