36bdd385475def8c1ffd0dabb01dea094e6de01f
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-12-05 10:43:01 +0900

    BuffViewReactivePresenterを修正

--- Assets/Scripts/BattleScene/InterfaceAdapter/ReactivePresenter/BuffViewReactivePresenter.cs
+++ Assets/Scripts/BattleScene/InterfaceAdapter/ReactivePresenter/BuffViewReactivePresenter.cs
@@ -37,7 +37,12 @@ namespace BattleScene.InterfaceAdapter.ReactivePresenter
         private void StartPlayerBuffView(BuffCode buffCode, float rate)
         {
             var index = _toIndex.FromBuff(buffCode);
-            var buff = new BuffViewModel(index, Math.Sign(Mathf.Log(rate)));
+            // QUESTION: Mathf.Logに1であることが期待される浮動小数点数を渡す場合、常に0を返す保証はあるか
+            // バフの倍率の対数を求め、倍率が1より大きい場合正の整数を、倍率が1より小さい場合負の整数を、倍率が1の場合は0を返す。
+            // ただし浮動小数点数を使用しているため、1を想定しているにもかかわらず、わずかに1より前後している可能性がある。
+            // そのため、Mathf.Approximatelyメソッドを用いて、倍率がほぼ1である場合、三項演算子でint型の0を返している。
+            var normalizeRate = Mathf.Approximately(rate, 1.0f) ? 0 : Mathf.Log(rate);
+            var buff = new BuffViewModel(index, Math.Sign(normalizeRate));
             _playerStatusView.StartPlayerBuffView(buff);
         }
     }
