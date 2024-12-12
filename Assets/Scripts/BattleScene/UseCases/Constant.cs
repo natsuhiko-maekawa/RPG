acfc75c5d25628639343bc0b3bad993467352ac9
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-12-12 15:30:53 +0900

    定数の実装を修正

--- Assets/Scripts/BattleScene/UseCases/Constant.cs
+++ Assets/Scripts/BattleScene/UseCases/Constant.cs
@@ -1,7 +1,9 @@
 namespace BattleScene.UseCases
 {
-    internal static class Constant
+    // constのバージョニング問題 (以下のページを参照) を避けるために、定数をget-only プロパティで定義している。
+    // https://ufcpp.net/study/csharp/sp_const.html#versioning
+    public static class Constant
     {
-        public const int MaxEnemyCount = 4;
+        public static int MaxEnemyCount { get; } = 4;
     }
 }
