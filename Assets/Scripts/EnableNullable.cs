24b7fd72311d4b83689255eb96aa004eaa1b0f76
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-12-13 00:05:52 +0900

    プロジェクトをNull許容参照型に設定するスクリプトを修正

--- Assets/Scripts/EnableNullable.cs
+++ Assets/Scripts/EnableNullable.cs
@@ -1,46 +1,34 @@
 using System;
-using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using UnityEditor;
-using UnityEditor.Compilation;
-using UnityEditor.Experimental;
-using UnityEditorInternal;
 
 public class EnableNullable : AssetPostprocessor
 {
-    private static readonly string[] DisableNullableAssemblyNameList = {
+    private static readonly string[] DisableNullableAssemblyNameArray = {
         "DataAccess.BattleScene",
         "Debug.BattleScene",
         "Framework.BattleScene",
         "View.LoadingScene"
     };
-        
+
     /// <summary>
-    /// UnityでNull許容参照型を利用可能にするために.csprojに追記を行うクラス。以下の記事を参考にした。<br/>
-    /// <see href="https://blog.kyubuns.dev/entry/2020/12/16/172335">Unity2020.2でnull許容参照型を使う</see>
+    /// UnityでNull許容参照型を利用可能にするために.csprojに追記を行うクラス。
     /// </summary>
     private static string OnGeneratedCSProject(string path, string content)
     {
-        var assemblyName = Path.GetFileNameWithoutExtension(path);
-        var assemblyDefinitionFilePath = CompilationPipeline.GetAssemblyDefinitionFilePathFromAssemblyName(assemblyName);
-        if (string.IsNullOrWhiteSpace(assemblyDefinitionFilePath)) return content;
-
-        var assemblyDefinition = EditorResources.Load<AssemblyDefinitionAsset>(assemblyDefinitionFilePath);
-        if (DisableNullableAssemblyNameList
-                .Any(x => assemblyDefinition.text.Contains($"\"name\": \"{x}\""))) return content;
-
         var document = XDocument.Parse(content);
-        if (document.Root == null) throw new Exception($"document.Root == null");
+        var root = document.Root ?? throw new InvalidOperationException();
+        var xNamespace = root.GetDefaultNamespace();
 
-        XNamespace xNamespace = "http://schemas.microsoft.com/developer/msbuild/2003";
+        var assemblyName = root.Descendants(xNamespace + "AssemblyName").Single().Value;
+        if (DisableNullableAssemblyNameArray.Contains(assemblyName)) return content;
 
-        // ReSharper disable once PossibleNullReferenceException
-        var propertyGroup = document
-            .Element(xNamespace + "Project")
-            .Elements(xNamespace + "PropertyGroup")
-            .First();
+        var propertyGroup = root.Descendants(xNamespace + "PropertyGroup").First();
         propertyGroup.Add(new XElement(xNamespace + "Nullable", "enable"));
+
+        var noWarn = root.Descendants(xNamespace + "NoWarn").Single();
+        noWarn.Value += ",8632";
         return document.ToString();
     }
 }
