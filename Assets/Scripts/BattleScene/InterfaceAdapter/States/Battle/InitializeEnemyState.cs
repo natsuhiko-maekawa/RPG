17eb22a968d5790bf1a54db767d9d10ed039c954
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-12-13 14:30:11 +0900

    CreateEnemyメソッドの戻り値を配列型に修正

--- Assets/Scripts/BattleScene/InterfaceAdapter/States/Battle/InitializeEnemyState.cs
+++ Assets/Scripts/BattleScene/InterfaceAdapter/States/Battle/InitializeEnemyState.cs
@@ -22,7 +22,7 @@ namespace BattleScene.InterfaceAdapter.States.Battle
         public override async void Start()
         {
             var enemyList = _useCase.CreateEnemy();
-            _enemyImageView.StartAnimation(enemyList.Count);
+            _enemyImageView.StartAnimation(enemyList.Length);
             await _enemyImageView.SetImage(enemyList);
             _useCase.Initialize();
             Context.TransitionTo(_turnState);
--- Assets/Scripts/BattleScene/UseCases/UseCase/InitializeEnemyUseCase.cs
+++ Assets/Scripts/BattleScene/UseCases/UseCase/InitializeEnemyUseCase.cs
@@ -4,6 +4,7 @@ using BattleScene.Domain.DataAccess;
 using BattleScene.Domain.Entity;
 using BattleScene.Domain.Id;
 using BattleScene.UseCases.IService;
+using Common;
 using Utility;
 using static BattleScene.Domain.Code.CharacterTypeCode;
 
@@ -26,12 +27,12 @@ namespace BattleScene.UseCases.UseCase
             _enemySelector = enemySelector;
         }
 
-        public IReadOnlyList<CharacterEntity> CreateEnemy()
+        public CharacterEntity[] CreateEnemy()
         {
             // TODO: 仮の値を設定している。
             var enemyTypeCodeArray = new[] { Bee, Dragon, Mantis, Shuten, Slime };
             _enemySelector.SelectEnemy(enemyTypeCodeArray, _enemyList);
-            return _enemyList;
+            return _enemyList.ToArray();
         }
 
         public void Initialize()
