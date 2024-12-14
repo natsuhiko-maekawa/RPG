ed1374aacd47c259df6f24426c917cdb323abf62
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-12-15 07:39:55 +0900

    キャラクターが死亡したとき、状態異常をリセットするよう修正

--- Assets/Scripts/BattleScene/UseCases/UseCases/CharacterDeadUseCase.cs
+++ Assets/Scripts/BattleScene/UseCases/UseCases/CharacterDeadUseCase.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using BattleScene.Domain.Codes;
 using BattleScene.Domain.Entities;
 using BattleScene.UseCases.IServices;
 
@@ -6,18 +9,37 @@ namespace BattleScene.UseCases.UseCases
 {
     public class CharacterDeadUseCase
     {
+        private readonly IAilmentResetService _ailmentReset;
         private readonly IDeadCharacterService _deadCharacter;
+        private readonly IDestroyResetService _destroyReset;
 
         public CharacterDeadUseCase(
-            IDeadCharacterService deadCharacter)
+            IDeadCharacterService deadCharacter,
+            IAilmentResetService ailmentReset,
+            IDestroyResetService destroyReset)
         {
             _deadCharacter = deadCharacter;
+            _ailmentReset = ailmentReset;
+            _destroyReset = destroyReset;
         }
 
         public bool IsPlayerDeadInThisTurn() => _deadCharacter.IsPlayerDeadInThisTurn();
         public bool IsAllEnemyDead() => _deadCharacter.IsAllEnemyDead();
         public IReadOnlyList<CharacterEntity> GetDeadCharacterInThisTurn()
             => _deadCharacter.GetDeadCharacterInThisTurn();
-        public void ConfirmedDead() => _deadCharacter.ConfirmedDead();
+        public void ConfirmedDead()
+        {
+            var deadCharacterArray = _deadCharacter.GetDeadCharacterInThisTurn();
+            var ailmentCodeArray = Enum.GetValues(typeof(AilmentCode))
+                .Cast<AilmentCode>()
+                .Where(y => y != AilmentCode.NoAilment)
+                .ToArray();
+            var resetAilmentLookup = deadCharacterArray
+                .SelectMany(_ => ailmentCodeArray, (x, y) => (x, y))
+                .ToLookup(x => x.x.Id, x => x.y);
+            _ailmentReset.Reset(resetAilmentLookup);
+
+            _deadCharacter.ConfirmedDead();
+        }
     }
 }
