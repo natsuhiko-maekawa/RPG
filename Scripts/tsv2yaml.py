a2d01aa70365ddd1cb489963d0d4c374b205b5ef
NatsuhikoMaekawa <natsuhiko.maekawa@gmail.com>
2024-12-16 11:43:11 +0900

    出力するファイル名を修正

--- Scripts/tsv2yaml.py
+++ Scripts/tsv2yaml.py
@@ -3,9 +3,10 @@ import sys
 import pandas as pd
 import yaml
 
-tsv_path = sys.argv[1]
-df = pd.read_csv("source/tsv/" + tsv_path, sep="\t")
+tsv_name = sys.argv[1]
+df = pd.read_csv("source/tsv/" + tsv_name, sep="\t")
 json_data = df.to_dict(orient="records")
 
-with open("output/yaml/output.yaml", "w", encoding="utf-8") as f:
+filename = tsv_name.split(".")[0]
+with open(f"output/yaml/{filename}.yaml", "w", encoding="utf-8") as f:
     yaml.dump(json_data, f, default_flow_style=False)
